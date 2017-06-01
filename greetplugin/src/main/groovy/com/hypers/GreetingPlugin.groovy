7f8b5b6a05e48ad556a868568cb88e08c36d8b95
renbo <renbo.ma@hypers.com>
2017-06-01 15:39:10 +0800

    update

--- greetplugin/src/main/groovy/com/hypers/GreetingPlugin.groovy
+++ greetplugin/src/main/groovy/com/hypers/GreetingPlugin.groovy
@@ -1,5 +1,6 @@
 package com.hypers
 
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,6 +8,20 @@ class GreetingPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+//        NamedDomainObjectContainer<Person> persons = project.container(Person)
+//        Team team = new Team(persons)
+//        project.extensions.add('team', team)
+//
+//        project.task('showTeam') {
+//            group 'hypers'
+//            doLast {
+//                def teams = project.extensions.getByName('team')
+//
+//                println teams.toString()
+//            }
+//        }
+
+
         project.extensions.create('greetingExt', GreetingPluginExtension)
         project.task('sayHi') {
             group 'hypers'
@@ -21,4 +36,50 @@ class GreetingPlugin implements Plugin<Project> {
 
 class GreetingPluginExtension {
     String message = 'HHHHHH from extension'
+}
+
+class Person {
+    String name;
+    int age;
+    String sex;
+
+    public Person(String name) {
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
+
+class Team {
+    NamedDomainObjectContainer<Person> persons;
+
+    String name;
+
+    int count;
+
+    public Team(NamedDomainObjectContainer<Person> persons) {
+        this.persons = persons;
+    }
+
+    def persons(Closure closure) {
+        persons.configure(closure)
+    }
+
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "count=" + count +
+                ", persons=" + persons +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
