e87b788d5a9b4e6ab3699c0617173f6b3dc1eb38
renbo <renbo.ma@hypers.com>
2017-09-29 18:32:47 +0800

    add plugin RyanInjectPlugin

--- greetplugin/src/main/groovy/com/hypers/Inject.groovy
+++ greetplugin/src/main/groovy/com/hypers/Inject.groovy
@@ -8,18 +8,16 @@ import org.objectweb.asm.commons.AdviceAdapter
 public class Inject {
 
     static File injectClass(File dir, File inputFile, Context context) {
-        System.out.println("dir.absolutePath = $dir.absolutePath")
-        // \app\build\intermediates\classes\apple\release
-        //通常buildTypes 都会存在。。
-        //如果productFlavors不存在的时候..那这个值就是classes
-        String buildTypes = dir.name
-        String productFlavors = dir.parentFile.name
-        File outputDir = new File(context.temporaryDir.getParentFile(),
-                "$productFlavors\\$buildTypes")
+        logE("dir.absolutePath = $dir.absolutePath")
+        logE("context.temporaryDIr = $context.temporaryDir.absolutePath")
+        logE("inputFile.absolutePath = $inputFile.absolutePath")
+        def outputFilePath = context.temporaryDir.parentFile.absolutePath + "\\" + inputFile.absolutePath.split("\\\\classes\\\\")[1]
+        logE("outputFilePath = $outputFilePath")
+        File outputDir = new File(outputFilePath.replace(inputFile.name, ""))
         if (!outputDir.exists()) {
             outputDir.mkdirs()
         }
-        File outputFile = new File(outputDir.absolutePath + "\\$inputFile.name")
+        File outputFile = new File(outputFilePath)
         if (!outputFile.exists()) {
             outputFile.createNewFile()
         }
@@ -29,6 +27,12 @@ public class Inject {
 
         FileUtils.writeByteArrayToFile(outputFile, bytes)
         return outputFile
+    }
+
+    static def logE(str) {
+        if (true) {
+            System.err.println("Inject : $str")
+        }
     }
 
     private static byte[] injectTimeWhenInit(InputStream inputStream) throws IOException {
--- greetplugin/src/main/groovy/com/hypers/RyanInjectPlugin.groovy
+++ greetplugin/src/main/groovy/com/hypers/RyanInjectPlugin.groovy
@@ -9,39 +9,42 @@ import com.android.build.gradle.internal.transforms.ProGuardTransform
 import groovy.io.FileType
 import org.apache.commons.codec.digest.DigestUtils
 import org.apache.commons.io.FileUtils
+import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
+import proguard.*
 
 import javax.xml.crypto.dsig.TransformException
 
 public class RyanInjectPlugin implements Plugin<Project> {
     //variant 对应的 混淆配置文件集合
+    Map<String, List<File>> mProguardConfigFile = new HashMap<>()
+
     Project mProject
-    Map<String, List<File>> mProguardConfigFile = new  HashMap<>()
 
     public void apply(Project project) {
         mProject = project
+        MyTransform transform
         project.afterEvaluate {
             def flavorsAndTypes = getProductFlavorsBuildTypes(project)
             flavorsAndTypes.each { item ->
                 copyMappingFile(project, item)
-//                doDex(project)
             }
         }
-        project.tasks.create('printProguardConfigFile') << {
-            mProguardConfigFile.each {
-                logE("key = $it.key")
-                it.value.each {
-                    logE("value = $it.absolutePath")
-                }
-            }
+        Task ryanDex = project.tasks.create('ryanDex') {
+            group 'ryan'
+            description 'dex specified class'
+        }
+        ryanDex.doLast {
+            prepareDex(project)
         }
         project.extensions.create('RYAN', MyExtension, project)
         def isApp = project.plugins.hasPlugin(AppPlugin);
         if (isApp) {
             //找到app 而不是library
             def android = project.extensions.getByType(AppExtension)
-            def transform = new MyTransform(project)
+            transform = new MyTransform(project)
             android.registerTransform(transform)
         }
     }
@@ -55,15 +58,6 @@ public class RyanInjectPlugin implements Plugin<Project> {
     def log(str) {
         if (false) {
             mProject.logger.error("log: $str")
-        }
-    }
-
-
-    def doDex(Project project) {
-        def modifiedClassDir
-        def flavorsAndTypes = getProductFlavorsBuildTypes(project)
-        flavorsAndTypes.each {
-            logE("flavorsAndTypes = $it")
         }
     }
 
@@ -91,30 +85,34 @@ public class RyanInjectPlugin implements Plugin<Project> {
         return composedList
     }
 
-    def copyMappingFile(Project project, String flavorsAndTypes) {
+    String capitalize(String flavorsAndTypes) {
         String[] arrays = flavorsAndTypes.split("\\\\")
-        def partMappingPath = ""
-        def i = 1
+//        def partMappingPath = ""
+//        def i = 1
         def changedFlavorsAndTypes = ""
         arrays.each {
-
             changedFlavorsAndTypes += it.replace(it.substring(0, 1), it.substring(0, 1).toUpperCase())
-            partMappingPath += it
-            if (i < arrays.length) {
-                partMappingPath += "/"
-            }
-            i++
+//            partMappingPath += it
+//            if (i < arrays.length) {
+//                partMappingPath += "/"
+//            }
+//            i++
         }
-        logE("changed flavorsAndTypes = $changedFlavorsAndTypes")
-        logE("partMappingPath = $partMappingPath")
+        changedFlavorsAndTypes
+    }
+
+    def copyMappingFile(Project project, String flavorsAndTypes) {
+        def changedFlavorsAndTypes = capitalize(flavorsAndTypes)
+        logE("changedFlavorsAndTypes = $changedFlavorsAndTypes")
+        logE("flavorsAndTypes = $flavorsAndTypes")
         def proguardTask = project.tasks.findByName("transformClassesAndResourcesWithProguardFor${changedFlavorsAndTypes}")
         if (proguardTask) {
-            logE("transformClassesAndResourcesWithProguardFor${changedFlavorsAndTypes} exsit")
+            logE("transformClassesAndResourcesWithProguardFor${changedFlavorsAndTypes} can be find")
             proguardTask.doLast {
-                def mapFile = new File("$project.buildDir/outputs/mapping/$partMappingPath/mapping.txt")
+                def mapFile = new File("$project.buildDir/outputs/mapping/$flavorsAndTypes/mapping.txt")
                 logE(mapFile.exists() ? "mapFile exist" : "mapFile not exist")
                 logE("mapFile path = $mapFile.absolutePath")
-                def mapCopyDir = new File("$project.buildDir/outputs/ryan/$partMappingPath")
+                def mapCopyDir = new File("$project.buildDir/outputs/ryan/$flavorsAndTypes")
                 if (!mapCopyDir.exists()) {
                     mapCopyDir.mkdirs()
                 }
@@ -123,7 +121,7 @@ public class RyanInjectPlugin implements Plugin<Project> {
                     mapCopyFile.createNewFile()
                 }
                 FileUtils.copyFile(mapFile, mapCopyFile)
-                List<File> fileList = getProguardConfigFile(project, proguardTask, partMappingPath)
+                List<File> fileList = getProguardConfigFile(proguardTask)
                 mProguardConfigFile.put(flavorsAndTypes, fileList)
             }
 
@@ -132,23 +130,167 @@ public class RyanInjectPlugin implements Plugin<Project> {
 
     /**
      *
-     * @param project
      * @param proguardTask 实际是一个TransformTask...(输出metaClass)
-     * @return
+     * @return List < File >
      */
-    def getProguardConfigFile(Project project, TransformTask proguardTask, String partPath) {
+    def getProguardConfigFile(TransformTask proguardTask) {
+        //proguardTask 存在一个方法获取transform
+        ProGuardTransform proGuardTransform = proguardTask.getTransform()
+        //获取所有的混淆的配置文件。。 这个方法是ProguardTRansform 的接口中的方法
+        //代码好像不提示。。。 但是 我打出来之后是可以使用
+        proGuardTransform.getAllConfigurationFiles()
+    }
+
+    def getMappingFile(Project project, String flavorAndType) {
+        def TAG = "getMappingFile :"
         def mExtension = project.extensions.findByName('RYAN') as MyExtension
-        def oldDir = new File(mExtension.oldDir)
-        if (oldDir.exists()) {
-            logE("oldDir exists")
-            def mappingFile = new File("$oldDir.absolutePath/$partPath/mapping.txt")
-            //proguardTask 存在一个方法获取transform
-            ProGuardTransform proGuardTransform = proguardTask.getTransform()
-            //获取所有的混淆的配置文件。。 这个方法是ProguardTRansform 的接口中的方法
-            //代码好像不提示。。。 但是 我打出来之后是可以使用
-            proGuardTransform.getAllConfigurationFiles()
+        def mappingDir = new File(mExtension.oldDir)
+        if (mappingDir.exists()) {
+            logE("$TAG mappingDir exist")
+            logE("$TAG $mappingDir.absolutePath/$flavorAndType/mapping.txt")
+            def mappingFile = new File("$mappingDir.absolutePath/$flavorAndType/mapping.txt")
+            logE("$TAG mappingFile path = $mappingFile.absolutePath")
+            mappingFile
+        }
+
+    }
+
+    def prepareDex(Project project) {
+        def TAG = 'method dex:  '
+        List<String> flavorsAndTypes = getProductFlavorsBuildTypes(project)
+        flavorsAndTypes.each { item ->
+            File classDir = new File("$project.buildDir\\tmp\\$item")
+            if (classDir.exists() && classDir.listFiles().size() > 0) {
+                logE("$TAG $item classDir has subFile or dir")
+                def sdkDir = getSdkDir(project)
+                logE("$TAG sdkDir = $sdkDir")
+                if (sdkDir) {
+                    def changedFlavorsAndTypes = capitalize(item)
+                    def proguardTask = project.tasks.
+                            findByName("transformClassesAndResourcesWithProguardFor${changedFlavorsAndTypes}")
+                    if (proguardTask) {
+                        logE("$TAG proguardTask can be find")
+                        def mappingFile = getMappingFile(project, item)
+                        //混淆的配置
+                        Configuration configuration = new Configuration()
+                        //使用混合的类名，这样不同的类混淆后将使用同一类名
+                        configuration.useMixedCaseClassNames = false
+                        configuration.programJars = new ClassPath()
+                        configuration.libraryJars = new ClassPath()
+                        //应用mapping文件
+                        configuration.applyMapping = mappingFile
+                        //打开日志
+                        configuration.verbose = true
+                        //输出配置文件
+                        configuration.printConfiguration = new File("$classDir.absolutePath/dump.txt")
+                        //不过滤没有引用的文件....应该是我们打的这些都都没有引用的文件，所以必须不过滤
+                        configuration.shrink = false
+                        //将android.jar和apache库加入依赖
+                        def compileSdkVersion = project.android.compileSdkVersion
+                        logE("compileSdkVersion = $compileSdkVersion")
+                        ClassPathEntry androidEntry = new ClassPathEntry(new File("$sdkDir/platforms/$compileSdkVersion/android.jar"), false)
+                        configuration.libraryJars.add(androidEntry)
+
+                        File apacheFile = new File("$sdkDir/$compileSdkVersion/platforms/optional/org.apache.http.legacy.jar")
+                        //android-23 以下才存在apache包
+                        if (apacheFile.exists()) {
+                            ClassPathEntry apacheEntry = new ClassPathEntry(apacheFile, false)
+                            configuration.libraryJars.add(apacheFile)
+                        }
+                        List<File> proguardLibFiles = getProguardLibFiles()
+                        //将MyTransform的所有输入文件都添加到混淆依赖jar
+                        if (proguardLibFiles) {
+                            ClassPathEntry jarFile
+                            proguardLibFiles.findAll { file ->
+                                jarFile = new ClassPathEntry(file, false)
+                                configuration.libraryJars.add(jarFile)
+                            }
+                        }
+
+                        //设置待dex未混淆的目录
+                        ClassPathEntry classPathEntry = new ClassPathEntry(classDir, false)
+                        configuration.programJars.add(classPathEntry)
+
+                        //定义混淆输出文件
+                        File proguardOutPut = new File("$project.buildDir.absolutePath/tmp/$item/proguard")
+                        //第二个参数表示是输出文件
+                        ClassPathEntry classPathEntryOut = new ClassPathEntry(proguardOutPut, true)
+                        configuration.programJars.add(classPathEntryOut)
+
+                        //外部定义的混淆文件的获取并应用
+                        def file = mProguardConfigFile.get(item)
+                        file.findAll { proguardFile ->
+                            logE("$TAG proguard外部定义的混淆文件 = $proguardFile.absolutePath")
+                            ConfigurationParser proguardParser = new ConfigurationParser(proguardFile, System.getProperties())
+                            try {
+                                proguardParser.parse(configuration)
+                            } catch (Exception e) {
+                                logE(e.message)
+                            }
+                        }
+
+                        //执行混淆
+                        ProGuard proGuard = new ProGuard(configuration)
+                        proGuard.execute()
+
+                        classDir = proguardOutPut
+
+                    }
+
+                    dex(project, sdkDir, classDir)
+
+
+                } else {
+                    logE("$TAG android sdk dir not defined")
+                }
+            }
         }
     }
+
+    def dex(Project project, String sdkDir, File classDir) {
+        def TAG = "dex :　"
+        logE("$TAG dex begining")
+        def cmdExt = Os.isFamily(Os.FAMILY_WINDOWS) ? '.bat' : ''
+        def stdout = new ByteArrayOutputStream()
+        project.exec {
+            commandLine "${sdkDir}/build-tools/${project.android.buildToolsVersion}/dx${cmdExt}",
+                    '--dex',
+                    "--output=${new File(classDir, 'ryan_dex.jar').absolutePath}",
+                    "${classDir.absolutePath}"
+            standardOutput = stdout
+        }
+        def error = stdout.toString().trim()
+        if (error) {
+            logE("$TAG dex error = $error")
+        }
+    }
+
+    def getProguardLibFiles() {
+        List<File> proguardLibFiles = new ArrayList<>()
+        File output = new File(mProject.buildDir.absolutePath + "\\tmp\\libFiles.txt")
+        output.withDataInputStream { input ->
+            input.eachLine { path ->
+                logE("proguardLibFiels = $path")
+                proguardLibFiles.add(new File(path))
+            }
+        }
+        proguardLibFiles
+    }
+
+    def getSdkDir(Project project) {
+        //确定sdkDir的路径
+        def sdkDir
+        Properties properties = new Properties()
+        File localProps = project.rootProject.file('local.properties')
+        if (localProps.exists()) {
+            properties.load(localProps.newDataInputStream())
+            sdkDir = properties.getProperty('sdk.dir')
+        } else {
+            sdkDir = System.getenv('ANDROID_HOME')
+        }
+    }
+
+
 }
 
 public class MyExtension {
@@ -163,6 +305,7 @@ public class MyExtension {
 
 
 class MyTransform extends Transform {
+    String TAG = com.hypers.MyTransform.class.getSimpleName()
     Project mProject
     MyExtension mExtension
 
@@ -201,40 +344,42 @@ class MyTransform extends Transform {
     void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
         super.transform(transformInvocation)
         int i = 0;
+        List<File> proguardLibfiles = new ArrayList<>();
+
         transformInvocation.inputs.each { TransformInput transformInput ->
             log("transformInput ${i++}")
             logW("directoryInputs")
-            transformInput.directoryInputs.findAll {
-                DirectoryInput directoryInput ->
-                    File dest = transformInvocation.outputProvider.getContentLocation(
-                            directoryInput.name, directoryInput.contentTypes, directoryInput.scopes, Format.DIRECTORY
-                    )
-                    HashMap<String, File> modifyMap = new HashMap<>();
-                    log("directoryInput path = $directoryInput.file.absolutePath")
-                    directoryInput.file.traverse(type: FileType.FILES, nameFilter: ~/.*\.class/) { File classFile ->
-                        log("classFile = $classFile.name")
-                        boolean needModify = filterFile(classFile)
-                        if (needModify) {
-                            try {
-                                File modifiedFile = Inject.injectClass(directoryInput.file, classFile, transformInvocation.context)
-                                modifyMap.put(classFile.absolutePath.replace(directoryInput.file.absolutePath, ""), modifiedFile)
-                            } catch (Exception e) {
-                                log(e.message)
-                            }
-                            log("over")
+            transformInput.directoryInputs.findAll { DirectoryInput directoryInput ->
+                File dest = transformInvocation.outputProvider.getContentLocation(
+                        directoryInput.name, directoryInput.contentTypes, directoryInput.scopes, Format.DIRECTORY
+                )
+                HashMap<String, File> modifyMap = new HashMap<>();
+                logW("$TAG directoryInput path = $directoryInput.file.absolutePath")
+                proguardLibfiles.add(directoryInput.file)
+                directoryInput.file.traverse(type: FileType.FILES, nameFilter: ~/.*\.class/) { File classFile ->
+                    log("classFile = $classFile.name")
+                    boolean needModify = filterFile(classFile)
+                    if (needModify) {
+                        try {
+                            File modifiedFile = Inject.injectClass(directoryInput.file, classFile, transformInvocation.context)
+                            modifyMap.put(classFile.absolutePath.replace(directoryInput.file.absolutePath, ""), modifiedFile)
+                        } catch (Exception e) {
+                            log(e.message)
                         }
+                        log("over")
                     }
-                    log("dest.absolute = $dest.absolutePath")
-                    FileUtils.copyDirectory(directoryInput.file, dest);
-                    modifyMap.each {
-                        log("key = $it.key")
-                        log("value = $it.value.absolutePath")
-                        File targetFile = new File(dest.absolutePath + "\\$it.key")
-                        if (targetFile.exists()) {
-                            targetFile.delete()
-                        }
-                        FileUtils.copyFile(it.value, targetFile)
+                }
+                log("dest.absolute = $dest.absolutePath")
+                FileUtils.copyDirectory(directoryInput.file, dest);
+                modifyMap.each {
+                    log("key = $it.key")
+                    log("value = $it.value.absolutePath")
+                    File targetFile = new File(dest.absolutePath + "\\$it.key")
+                    if (targetFile.exists()) {
+                        targetFile.delete()
                     }
+                    FileUtils.copyFile(it.value, targetFile)
+                }
             }
 
             logW("jarInputs")
@@ -248,9 +393,23 @@ class MyTransform extends Transform {
                         destName + "_" + hexName, jarInput.contentTypes, jarInput.scopes, Format.JAR
                 )
                 //TODO 处理JAR进行字节码注入
+                logW("$TAG jarinput path = $jarInput.file.absolutePath")
+                proguardLibfiles.add(jarInput.file)
                 FileUtils.copyFile(jarInput.file, dest)
             }
+        }
 
+        if (proguardLibfiles && proguardLibfiles.size()) {
+            File output = new File(mProject.buildDir.absolutePath + "\\tmp\\libFiles.txt")
+            if (!output.exists()) {
+                output.createNewFile()
+            }
+            output.withDataOutputStream { strem ->
+                proguardLibfiles.findAll { File file ->
+                    strem.write("$file.absolutePath\n".getBytes())
+                    strem.flush()
+                }
+            }
         }
     }
 
